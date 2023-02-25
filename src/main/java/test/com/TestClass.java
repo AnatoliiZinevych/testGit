730859a4bc0226998fec3deb941313b95c335aee
Anatolii <anatolii@gmail.com>
2023-02-25 16:52:40 +0200

    added some code

---
+++ src/main/java/test/com/TestClass.java
@@ -0,0 +1,16 @@
+package test.com;
+
+import org.junit.jupiter.api.Test;
+
+public class TestClass {
+
+    @Test
+    public void Test1() {
+        System.out.println("Hello World!");
+    }
+
+    @Test
+    public void Test2() {
+        System.out.println("Replace from test.txt");
+    }
+}
